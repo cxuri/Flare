29a5f154071bcd50bd2e190574280ab80d7bcdb6
Dhanesh U pai <dhaneshupai7@gmail.com>
2025-01-10 16:28:31 +0530

    added constructor, and now the function returns value

--- lib/flare.dart
+++ lib/flare.dart
@@ -3,13 +3,18 @@ import 'flare_platform_interface.dart';
 
 class Flare{
   String sha256 = "";
-  Future<void> main() async {
+
+  Flare(this.sha256);
+
+  Future<bool> verify() async {
     try {
       final result = await FlarePlatform.instance.validateAppIntegrity(sha256);
       if (result == true) {
         print("App integrity verified");
+        return true;
       } else {
         print("App integrity verification failed!");
+        return false; 
       }
     } catch (e) {
       print('Error while validating app integrity: $e');
