c71a45ebcd1b813aa87f7bb8cd8d7c7f795446de
Dhanesh U Pai <rootrcz@gmail.com>
2025-01-29 20:57:36 +0530

    fixes

--- android/src/main/java/com/example/flare/FlarePlugin.java
+++ android/src/main/java/com/example/flare/FlarePlugin.java
@@ -1,5 +1,6 @@
 package com.example.flare;
-import com.example.flare.IntegrityCheck;
+
+import android.content.Context;
 
 import androidx.annotation.NonNull;
 
@@ -11,36 +12,43 @@ import io.flutter.plugin.common.MethodChannel.Result;
 
 /** FlarePlugin */
 public class FlarePlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private MethodChannel channel;
+    private MethodChannel channel;
 
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flare");
-    channel.setMethodCallHandler(this);
-  }
-
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    
-    switch (call.method) {
-      case "validateAppIntegrity":
-        IntegrityCheck integrityChecker = new IntegrityCheck(context);
-        String sha256 = call.argument("sha");
-        boolean isValid = integrityChecker.validateAppIntegrity(sha256);
-        result.success(isValid);
-        break;
-      
-      default:
-        result.notImplemented();
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flare");
+        channel.setMethodCallHandler(this);
     }
-  }
 
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
-  }
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        switch (call.method) {
+            case "validateAppIntegrity":
+                // Get the application context
+                Context context = flutterPluginBinding.getApplicationContext();
+                
+                // Instantiate IntegrityCheck with context
+                IntegrityCheck integrityChecker = new IntegrityCheck(context);
+                
+                // Get the SHA256 checksum from the method call arguments
+                String sha256 = call.argument("sha");
+                
+                try {
+                    // Validate app integrity by comparing the hashes
+                    boolean isValid = integrityChecker.validateAppIntegrity(sha256);
+                    result.success(isValid);
+                } catch (IOException e) {
+                    result.error("IO_ERROR", "Error reading APK file", null);
+                }
+                break;
+
+            default:
+                result.notImplemented();
+        }
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+    }
 }
